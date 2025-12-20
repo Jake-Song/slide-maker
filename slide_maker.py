e807e4acd60981d47f0399b68efb9171ccb41c5f
Jake Song <mubok0212@gmail.com>
2025-12-20 23:57:18 +0900

    WIP: add dynamic parallel for performance

--- slide_maker.py
+++ slide_maker.py
@@ -5,8 +5,10 @@ import base64
 import requests
 from pathlib import Path
 from langgraph.graph import StateGraph, START, END, add_messages
-from typing import Annotated, TypedDict
+from typing import Annotated, TypedDict, List
+import operator
 from langchain.messages import AnyMessage, AIMessage
+from langgraph.types import Send
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -86,9 +88,47 @@ def generate_text(prompt: str, model: str) -> dict:
     result = response.json()
     return result
 
+def generate_structured_output(prompt: str, model: str, response_format: dict) -> dict:
+    url = "https://openrouter.ai/api/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "model": model,
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "response_format": response_format
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    result = response.json()
+    return result
+
+from pydantic import BaseModel, Field
+
+class Slide(BaseModel):
+    title: str = Field(
+        description="title for this slide.",
+    )
+    text: str = Field(
+        description="text for this slide.",
+    )
+
+class Slides(BaseModel):
+    slides: List[Slide] = Field(
+        description="Slides to present the document in a presentation.",
+    )
+
 class GraphState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
 
+class WorkerState(TypedDict):
+    plan: Slide
+    slides: Annotated[list[Slide], operator.add]
 
 PLANNER_PROMPT = """
     You are a planner to make slides.
@@ -110,18 +150,76 @@ GENERATE_PROMPT = """
    
     """
 
+response_format = {
+    "type": "json_schema",
+    "json_schema": {
+        "name": "presentation_slides",
+        "strict": True,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "slides": {
+                    "type": "array",
+                    "description": "Slides to present the document in a presentation.",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "title": {
+                                "type": "string",
+                                "description": "title for this slide."
+                            },
+                            "text": {
+                                "type": "string",
+                                "description": "text for this slide."
+                            }
+                        },
+                        "required": ["title", "text"],
+                        "additionalProperties": False
+                    }
+                }
+            },
+            "required": ["slides"],
+            "additionalProperties": False
+        }
+    }
+}
+
 # nodes
 def planner(state: GraphState) -> GraphState:
     messages = state["messages"][-1].content
-    res = generate_text(PLANNER_PROMPT.format(texts=messages), model="google/gemini-3-pro-preview")
+    res = generate_structured_output(
+        PLANNER_PROMPT.format(texts=messages), 
+        model="google/gemini-3-pro-preview", 
+        response_format=response_format
+    )
     return {"messages": [AIMessage(content=res["choices"][0]["message"]["content"])]}
 
-def generate_slide(state: GraphState) -> GraphState:
-    plan = state["messages"][-1].content
-    res = generate_image(GENERATE_PROMPT.format(plan=plan), model="google/gemini-2.5-flash-image")
-    return {"messages": [AIMessage(content=res["choices"][0]["message"]["content"])]}
+def generate_slide(state: WorkerState) -> WorkerState:
+    plan = state["plan"]
+    res = generate_image(
+        GENERATE_PROMPT.format(plan=plan), 
+        model="google/gemini-2.5-flash-image"
+    )
+    return {"slides": [Slide(title=res["choices"][0]["message"]["content"])]}
+
+def synthesizer(state: GraphState):
+    """Synthesize full answer from queries"""
+
+    # List of completed sections
+    completed_slides = state["slides"]
+
+    # Format completed section to str to use as context for final sections
+    completed_slides = "\n\n---\n\n".join(completed_slides)
+
+    return {"final_answer": completed_slides}
 
 
+# Conditional edge function to create llm_call workers that each write a section of the report
+def assign_workers(state: GraphState):
+    """Assign a worker to each section in the plan"""
+
+    # Kick off query writing in parallel via Send() API
+    return [Send("generate_slide", {"plan": p}) for p in state["plan"]]
 # graph
 graph = StateGraph(GraphState)
 graph.add_node("planner", planner)
@@ -136,7 +234,7 @@ checkpointer = MemorySaver()
 app = graph.compile(checkpointer=checkpointer)
 
 if __name__ == "__main__":
-    sample = get_text("demo/sample.txt")
+    sample = get_text("texts/sample-2.txt")
   
     config={"configurable": {"thread_id": "1"}}
     for chunk in app.stream({"messages": [{"role": "user", "content": sample}]}, stream_mode="updates", config=config):
